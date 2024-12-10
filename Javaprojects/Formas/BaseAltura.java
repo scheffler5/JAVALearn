dda9bcfdc34047120c252a6c789f853f207953f6
Gabriel Lima Scheffler <44821976+scheffler5@users.noreply.github.com>
2024-12-10 00:49:02 -0300

    Create BaseAltura.java

---
+++ Javaprojects/Formas/BaseAltura.java
@@ -0,0 +1,25 @@
+public class BaseAltura implements CalcArea{
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+    public BaseAltura(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+    private double area;
+    @Override
+    public void area() {
+        this.area = area;
+            double altura = (lado1 *lado1)- ((lado1*lado1)/4);
+            area = (lado1 * altura)/2;
+            System.out.printf("Area: %f",area);
+    }
+
+    @Override
+    public void perimetro() {
+        
+    }
+    
+}
